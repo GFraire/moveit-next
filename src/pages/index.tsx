68a990235c8dde72d17b21011d580658212fe228
Gabriel Freire <gabrielfreiresa@gmail.com>
2021-02-23 23:32:29 -0300

    CountDown in progress

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { CountDown } from "../components/CountDown";
+
+import styles from "../styles/pages/Home.module.css"
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <CountDown />
+        </div>
+        <div>
+
+        </div>
+      </section>
+    </div>
+  )
+}
